eea27c7d249758cb65779490bff12fe2176ac31b
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-08-19 14:03:26 +0530

    bootdev: lesson 3

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
 
+from openai import OpenAI
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("OPENAPI_KEY")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ],
+)
+
+print(response.choices[0].message.content)
